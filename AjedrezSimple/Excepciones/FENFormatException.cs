a26a0d0e5e047a4985077ea54867d80f9f34d283
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2023-09-27 04:25:47 -0300

    Agregar Excepciones Personalizadas para FEN y PGN

---
+++ AjedrezSimple/Excepciones/FENFormatException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjedrezSimple {
+	class FENFormatException: FormatException {
+		public FENFormatException(): base("No se pudo decodificar la notación FEN ingresada porque tenía un formato inválido") {}
+	}
+}
---
+++ AjedrezSimple/Excepciones/PGNFormatException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace AjedrezSimple {
+	class PGNFormatException: FormatException {
+		public PGNFormatException(): base("No se pudo decodificar la notación PGN ingresada porque tenía un formato inválido") {}
+	}
+}
